bb954a0b9e3f612ec6c3ce779779812f0ecc9b24
HelenArkh <alena.arkh@gmail.com>
2021-03-11 11:17:52 +0300

    Основные коллекции часть 1

---
+++ Module13/Module13/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module13
+{
+    public class Contact
+    {
+        public Contact(string name, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
--- Module13/Module13/Program.cs
+++ Module13/Module13/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -9,47 +10,91 @@ namespace Module13
     {
         static void Main(string[] args)
         {
-            // Объявим ArrayList с элементами разных типов
-            var arrayList = new ArrayList()
-           {
-               1,
-               "Андрей ",
-               "Сергей ",
-               300,
-           };
-
-            // переменная для хранения суммы
-            int sum = 0;
-
-            // переменная для хранения текста.
-            // Можно было бы использовать String, но в случае когда необходимо выполнять много
-            // операций с одной строкой - лучше использовать класс StringBuilder
-            StringBuilder text = new StringBuilder();
-
-            // проходим список и проверяем элементы на соответствие типу
-            foreach (var element in arrayList)
+            // Запускаем бесконечный цикл
+            while (true)
             {
-                //   если целое число - увеличиваем счётчик
-                if (element is int)
-                {
-                    sum += (int)element;
-                }
+                Console.WriteLine("Введите текст:");
 
-                // если строка - добавляем текст из неё
-                if (element is string s)
+                // Сохраняем предложение в строку
+                var sentence = Console.ReadLine();
+                // сохраняем в массив char
+                var characters = sentence.ToCharArray();
+
+            var symbols = new HashSet<char>();
+
+            // добавляем во множество. Сохраняются только неповторяющиеся символы
+            foreach (var symbol in characters)
+                symbols.Add(symbol);
+
+                // Выводим результат
+                Console.WriteLine($"Всего {symbols.Count} уникальных символов");
+
+                // сохраняем знаки препинания в массив Char
+                var signs = new[] { ',', ' ', '.' };
+
+            // сохраняем числовые символы в массив Char
+            var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            //  Проверяем, есть ли цифры
+            bool containsNumbers = symbols.Overlaps(numbers);
+            Console.WriteLine($"Коллекция содержит цифры: {containsNumbers}");
+
+            // Отбрасываем знаки препинания и заново считаем
+            symbols.ExceptWith(signs);
+            Console.WriteLine($"Символов без знаков препинания: {symbols.Count}");
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        static void PrintCollection(SortedSet<char> ss, string s)
+        {
+            Console.WriteLine(s);
+            foreach (char ch in ss)
+                Console.Write(ch + " ");
+            Console.WriteLine("\n");
+        }
+
+        private static void AddUnique(Contact newContact, List<Contact> phoneBook)
+        {
+            bool alreadyExists = false;
+
+            // пробегаемся по списку и смотрим, есть ли уже с таким именем
+            foreach (var contact in phoneBook)
+            {
+                if (contact.Name == newContact.Name)
                 {
-                    text.Append(element);
+                    //  если вдруг находим  -  выставляем флаг и прерываем цикл
+                    alreadyExists = true;
+                    break;
                 }
             }
 
-            // результат
-            var result = new ArrayList() { sum, text.ToString() };
+            if (!alreadyExists)
+                phoneBook.Add(newContact);
 
-            // вывод
-            foreach (var elem in result)
-            {
-                Console.WriteLine(elem);
-            }
+            //  сортируем список по имени
+            phoneBook.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
+
+            foreach (var contact in phoneBook)
+                Console.WriteLine(contact.Name + ": " + contact.PhoneNumber);
+        }
+
+        private static void GetMissing(List<string> months, ArrayList missing)
+        {
+            // инициализируем массив для 7 нужных нам недостающих элементов
+            var missedArray = new string[7];
+
+            // извлекаем эти элементы из ArrayList, и копируем в массив
+            missing.GetRange(4, 7).CopyTo(missedArray);
+
+            // Добавляем наш массив в конец списка
+            months.AddRange(missedArray);
+
+            // смотрим, что получилось
+            foreach (var month in months)
+                Console.WriteLine(month);
         }
     }
 }
